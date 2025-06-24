e3e75b6f957bafe8c4e4cdf0b676533c6a595f14
Ana Guerreiro <2103229@estudantes.uab.pt>
2025-06-24 19:14:31 +0100

    merge de Ana com versão Tiago resolvida

--- lib/screens/game_menu.dart
+++ lib/screens/game_menu.dart
@@ -155,7 +155,8 @@ class _GameMenuState extends State<GameMenu> {
                   ],
                 ),
               ],
-            ),
+            );
+        },
       );
     }
     isMenuMusicAllowed = true;
